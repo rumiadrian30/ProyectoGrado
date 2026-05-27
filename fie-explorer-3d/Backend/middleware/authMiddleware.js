0d4729ca0c3682a22bf48373bdf8762443e48f3b
rumiadrian30 <91993007+rumiadrian30@users.noreply.github.com>
2026-05-27 14:04:06 -0500

    update

--- fie-explorer-3d/Backend/middleware/authMiddleware.js
+++ fie-explorer-3d/Backend/middleware/authMiddleware.js
@@ -18,21 +18,16 @@ module.exports = function authMiddleware(req, res, next) {
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-
     req.admin = {
       id: payload.userId,
       email: payload.email,
       role: payload.role,
     };
-
     next();
-
   } catch (err) {
-
     console.warn(
       `[AUTH] Token expirado o inválido → ${req.ip} → ${req.originalUrl}`
     )
-
     return res.status(401).json({
       error: 'Token inválido o expirado.'
     })
@@ -43,24 +38,19 @@ module.exports.optional = function authOptional(req, res, next) {
   const token =
     req.cookies?.token ||
     req.headers.authorization?.replace('Bearer ', '');
-
   if (token) {
     try {
       const payload = jwt.verify(token, process.env.JWT_SECRET);
-
       req.admin = {
         id: payload.userId,
         email: payload.email,
         role: payload.role
       };
-
     } catch {
-
       console.warn(
         `[AUTH] Token expirado o inválido → ${req.ip} → ${req.originalUrl}`
       )
     }
   }
-
   next();
 };
